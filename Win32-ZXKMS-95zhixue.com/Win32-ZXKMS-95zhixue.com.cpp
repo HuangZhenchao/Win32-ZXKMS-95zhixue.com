ff6ef7a590bd9f5872559bc651f0ec428236370c
hzc <1746565921@qq.com>
2019-02-18 04:43:47 +0800

    创建主窗体和侧边栏窗体，实现了配置文件的读取和写入，利用配置来保存用户的一些设置，同时保存程序上次退出时的状态

--- Win32-ZXKMS-95zhixue.com/Win32-ZXKMS-95zhixue.com.cpp
+++ Win32-ZXKMS-95zhixue.com/Win32-ZXKMS-95zhixue.com.cpp
@@ -1,22 +1,42 @@
 // Win32-ZXKMS-95zhixue.com.cpp : 定义应用程序的入口点。
 //
-
 #include "stdafx.h"
 #include "Win32-ZXKMS-95zhixue.com.h"
+#include <Shlwapi.h>
+#pragma comment(lib,"shlwapi.lib")
+
 
 #define MAX_LOADSTRING 100
 
+struct WNDINI {
+	int WndMainX;
+	int WndMainY;
+	int WndMainWidth;
+	int WndMainHeight;
+};
+WNDINI WndIni = { 0 };
+
 // 全局变量: 
-HINSTANCE hInst;                                // 当前实例
+HINSTANCE hInst;							// 当前实例
+HWND hWndMain;
+HWND hWndSide;
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
 
+WCHAR path[MAX_PATH];							//运行程序文件或dll所在的位置
+WCHAR IniPath[MAX_PATH];
+
+
 // 此代码模块中包含的函数的前向声明: 
-ATOM                MyRegisterClass(HINSTANCE hInstance);
+ATOM                RegWndMain(HINSTANCE hInstance);
+ATOM                RegWndSide(HINSTANCE hInstance);
+ATOM                RegWndCatalog(HINSTANCE hInstance);
+ATOM                RegWndFileList(HINSTANCE hInstance);
+ATOM                RegWndSpiltter(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
+int					WriteWndIni();					//用来写入Windows配置文件
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -30,8 +50,37 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // 初始化全局字符串
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_WIN32ZXKMS95ZHIXUECOM, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	//初始化path
+	HMODULE hModule = GetModuleHandleW(NULL);
+	GetModuleFileName(hModule, path, MAX_PATH);
+	// setting -> link add Shlwapi.lib   #include <Shlwapi.h>
+	PathRemoveFileSpec(path);
+	//初始化配置文件路径
+	wcscpy_s(IniPath, path);
+	wcscat_s(IniPath, L"\\wnd.ini");
 
+	//从配置文件取出结构体到WndIniRead
+	if (!GetPrivateProfileStruct(_T("Windows"), _T("Struct"), &WndIni, sizeof(WndIni), IniPath))
+	{
+		WndIni.WndMainX = CW_USEDEFAULT;
+		WndIni.WndMainY = 0;
+		WndIni.WndMainWidth = CW_USEDEFAULT;
+		WndIni.WndMainHeight = 0;
+	}
+
+
+	//用于测试字符串变量
+	WCHAR DebugOutputField[] = L"测试";
+	WCHAR DebugOutputInfo[] = L"字符串变量输出测试可行";
+	WCHAR DebugOutputFilePath[] = L"D:\\DebugOutputFile.ini";
+	WritePrivateProfileString(_T("Debug"), DebugOutputField, DebugOutputInfo, DebugOutputFilePath);
+
+	//wchar_t tempStr[10];
+	//_itow_s(WndIniRead.WndMainWidth, tempStr, 10);
+
+
+	RegWndMain(hInstance);
+	RegWndSide(hInstance);
     // 执行应用程序初始化: 
     if (!InitInstance (hInstance, nCmdShow))
     {
@@ -58,31 +107,53 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 //
-//  函数: MyRegisterClass()
+//  函数: RegWndMain()
 //
-//  目的: 注册窗口类。
+//  目的: 注册主窗口类。
 //
-ATOM MyRegisterClass(HINSTANCE hInstance)
+ATOM RegWndMain(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32ZXKMS95ZHIXUECOM));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WIN32ZXKMS95ZHIXUECOM);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32ZXKMS95ZHIXUECOM));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WIN32ZXKMS95ZHIXUECOM);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
+//
+//  函数: RegWndSide()
+//
+//  目的: 注册侧边栏窗口类。
+//
+ATOM RegWndSide(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
 
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = NULL;
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = CreateSolidBrush(RGB(42, 51, 60));
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"WndSide";
+	wcex.hIconSm = NULL;
+
+	return RegisterClassExW(&wcex);
+}
 //
 //   函数: InitInstance(HINSTANCE, int)
 //
@@ -97,16 +168,17 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 将实例句柄存储在全局变量中
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
+   hWndMain = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+	   WndIni.WndMainX, WndIni.WndMainY, WndIni.WndMainWidth, WndIni.WndMainHeight, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWndMain)
    {
       return FALSE;
    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+   ShowWindow(hWndMain, nCmdShow);
+   UpdateWindow(hWndMain);
 
    return TRUE;
 }
@@ -121,10 +193,27 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - 发送退出消息并返回
 //
 //
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+	case WM_CREATE:
+	{
+		
+		hWndSide = CreateWindowEx(NULL, L"WndSide", NULL, WS_CHILD | WS_VISIBLE,
+			0, 0, 50, 50, hWnd, NULL, hInst, NULL);
+		
+
+	}
+	case WM_SIZE:
+		{
+			RECT rect;
+			GetClientRect(hWnd,&rect);
+			int widthClient = rect.right - rect.left;  // 获得客户区宽度
+			int heightClient = rect.bottom - rect.top;  // 获得客户区高度
+			MoveWindow(hWndSide,0, 0, 50, heightClient,TRUE);
+		}
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -151,6 +240,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
     case WM_DESTROY:
+		WriteWndIni();
         PostQuitMessage(0);
         break;
     default:
@@ -177,4 +267,18 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
+}
+
+int WriteWndIni() 
+{
+	RECT rect;
+	GetWindowRect(hWndMain, &rect);
+	int widthWndMain = rect.right - rect.left;  // 获得主窗体宽度
+	int heightWndMain = rect.bottom - rect.top;  // 获得主窗体高度
+	WndIni.WndMainX = rect.left;
+	WndIni.WndMainY = rect.top;
+	WndIni.WndMainWidth = widthWndMain;
+	WndIni.WndMainHeight = heightWndMain;
+	return WritePrivateProfileStruct(_T("Windows"), _T("Struct"), &WndIni, sizeof(WndIni), IniPath);
+	
 }
